94be2191649401a4ae112843cb29ce260d4cd0b4
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-04-09 21:31:53 +0600

    Set up done

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
+)

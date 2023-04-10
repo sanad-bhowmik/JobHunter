d27d7eb9c937eccb390bed7e46d5935ffb5bcb0d
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-04-10 17:35:35 +0600

    rechat assignment mark done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,89 @@
 import React from 'react';
+import './Statistics.css'
+import {
+    ComposedChart,
+    Line,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    Area
+} from "recharts";
 
 const Statistics = () => {
+
+
+    const data = [
+        {
+            name: "A-01",
+            pv: 55,
+        },
+        {
+            name: "A-02",
+            pv: 57,
+        },
+        {
+            name: "A-03",
+            pv: 55,
+        },
+        {
+            name: "A-04",
+            pv: 60,
+        },
+        {
+            name: "A-05",
+            pv: 55,
+        },
+        {
+            name: "A-06",
+            pv: 60,
+        },
+        {
+            name: "A-07",
+            pv: 60,
+        }
+    ];
+
     return (
-        <div>
-            <h1>Statistics</h1>
+        <div className='container mx-auto mt-[40px] '>
+            <h1 className='assignment text-6xl underline text-center pb-20'>Assignment marks</h1>
+            <section className='flex gap-[20%]'>
+                <div className='chart'>
+                    <ComposedChart
+                        layout="vertical"
+                        width={800}
+                        height={500}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 20,
+                            bottom: 20,
+                            left: 50
+                        }}
+                    >
+                        <CartesianGrid stroke="#f5f5f5" />
+                        <XAxis type="number" />
+                        <YAxis dataKey="name" type="category" scale="band" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="pv" barSize={20} fill="#ff7300" />
+                    </ComposedChart>
+                </div>
+                <div>
+                    <ol className=''>
+                        <li className='color1'>Assignment-55</li>
+                        <li className='color2'>Assignment-57</li>
+                        <li className='color3'>Assignment-55</li>
+                        <li className='color4'>Assignment-60</li>
+                        <li className='color5'>Assignment-55</li>
+                        <li className='color6'>Assignment-60</li>
+                        <li className='color7'>Assignment-60</li>
+                    </ol>
+
+                </div>
+            </section>
         </div>
     );
 };

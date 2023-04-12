9fb729b3d1ff9bf83be58089fe245064c5f46e7e
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-04-12 22:27:25 +0600

    apply job design done

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,21 +1,8 @@
-// import React from 'react';
-
-// const AppliedJobs = () => {
-//     const jobDetails = JSON.parse(localStorage.getItem('jobDetails'));
-
-//     return (
-//         <div>
-//             <h2>{jobDetails.location}</h2>
-//             <p>{jobDetails.place}</p>
-//             <p>{jobDetails.salary}</p>
-//         </div>
-//     );
-// };
-
-// export default AppliedJobs;
 import React, { useEffect, useState } from 'react';
 import ApplySingle from '../ApplySingle/ApplySingle';
 import { getStoredData } from '../../utilities/addToJob'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faDollar } from '@fortawesome/free-solid-svg-icons'
 
 const AppliedJobs = () => {
     const [items, setItems] = useState([]);
@@ -35,17 +22,25 @@ const AppliedJobs = () => {
             </div>
             {items.map((item) => (
 
-                <div>
+                <div className='infoOfjob'>
                     {/* <h1>hello: {item.place}</h1> */}
 
-                    <div className="card card-side bg-base-100 shadow-xl h-[300px] container mx-auto">
-                        <figure className='company-img'><img className='h-[100px]  w-[240px] ml-11' src={item.CompanyLogo} alt="Movie" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">New movie is released!</h2>
-                            <p>Click the button to watch on Jetflix app.</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Watch</button>
+                    <div className="card border-solid border-2 border-stone-300 card-side bg-base-100 shadow-xl h-[300px] container mx-auto mb-6 mt-10">
+                        <figure className='company-img'><img className='h-[150px]  w-[180px] ml-11' src={item.CompanyLogo} alt="Movie" /></figure>
+                        <div className="card-body pl-10 ">
+                            {item.Jobpost && <h1 className='jobpost text-3xl font-bold text-stone-950'>{item.Jobpost}</h1>}
+                            {item.place && <h1 className='text-2xl text-stone-500'>{item.place}</h1>}
+                            <div className='flex gap-6'>
+                                {item.RemoteJob && <h1 className='p-2 text-xl border-solid border-2 border-pink-300 bg-purple-200 rounded-m text-stone-500'>{item.RemoteJob}</h1>}
+                                {item.FulltimeJob && <h1 className='p-2 text-xl border-solid border-2 border-pink-300 bg-purple-200 rounded-m text-stone-500'>{item.FulltimeJob}</h1>}
                             </div>
+                            <div className='flex gap-4 mt-4'>
+                                {item.location && <h1 className='text-stone-400'><FontAwesomeIcon icon={faLocationDot} />{item.location}</h1>}
+                                {item.salary && <h1 className='text-stone-400'><FontAwesomeIcon icon={faDollar} /><span className='font-bold'>Salary : </span>{item.salary}</h1>}
+                            </div>
+                        </div>
+                        <div className="card-actions mr-20 mt-[7%]">
+                            <button className="btn btn-primary nav-btn text-xl">View Details</button>
                         </div>
                     </div>
                 </div>
